13e1d2d701c8d44f760f1181daf1e8108c5af2c6
esomchi <georgevincent35@gmail.com>
2022-02-09 18:32:31 +0100

    my first commit

---
+++ js/index.js
@@ -0,0 +1,51 @@
+
+// const students = [
+//     {id:10001, name:'jennifer', dateOfBirt:2003},
+//     {id:10002, name:'weston', dateOfBirt:2001},
+//     {id:10003, name:'goodness', dateOfBirt:2000},
+//     {id:10004, name:'morgan', dateOfBirt:2008},
+//     {id:10005, name:'albert', dateOfBirt:2009}
+// ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let numberOfHours = Number(prompt("Enter the number of hours ",""));
+
+// let days = Math.trunc(numberOfHours/24);
+// let remainingHours = numberOfHours % 24;
+// let amount = 0;
+
+// if(days > 0){
+
+//     amount = amount + (days * 15);
+//     amount = amount + (remainingHours * 0.5);
+
+//     console.log(amount);
+// }else{
+
+//     if(remainingHours >=5){
+//         amount = 5 + (remainingHours-5) * 0.5;
+//         console.log(amount);
+//     }else{
+//         amount = remainingHours;
+//         console.log(amount);
+//     }
+// }
